c8b55ad27309102dd7078a2b69eebf32602101ef
Tiago Andrade e Silva <tiagonmas@users.noreply.github.com>
2011-12-18 12:06:24 +0000

    First checkin

---
+++ ListLinks/ListLinks/CmdLineOptions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Plossum.CommandLine;
+
+namespace ListLinks
+{
+
+    [CommandLineManager(ApplicationName = "\nList Links",
+        Copyright = "Lists all links that exist in a given page")]
+    class CmdLineOptions
+    {
+        [CommandLineOption(Description = "Displays this help text")]
+        public bool Help = false;
+
+        [CommandLineOption(Description = "Specifies the URL to look for", MinOccurs = 1)]
+        public string URL
+        {
+            get { return mURL; }
+            set
+            {
+                mURL = value;
+            }
+        }
+
+        [CommandLineOption(Description = "The number of internal links the crawler will follow", MinOccurs = 0)]
+        public int Max
+        {
+            get { return mMax; }
+            set
+            {
+                mMax = value;
+            }
+        }
+        private string mURL;
+        private int mMax=1;
+    }
+
+}
+
